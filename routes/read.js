7e37c731207be063d9ade490a05bbe3a9eb27dd3
Shercosta <70560582+Shercosta@users.noreply.github.com>
2022-12-28 21:19:48 +0700

    read section fixed

--- routes/read.js
+++ routes/read.js
@@ -1,20 +1,32 @@
 // READ for Blog Reading
-
+const _ = require("lodash");
 const router = require("express").Router();
+const { asyncMw } = require("express-asyncmw");
+const repository = require("../repository");
 
-router.get("/read/:blogParams", (req, res) => {
-  const requestedPost = req.params.blogParams;
-
-  article.forEach((news) => {
-    if (requestedPost === news.title) {
-      res.render("read", {
-        cssEjs: "read",
-        blogTitle: news.title,
-        blogImage: news.image,
-        blogContent: news.content,
-      });
-    }
+router.get("/read/:blogParams", async (req, res) => {
+  // const requestedPost = req.params.blogParams;
+  const requestedPost = await repository.blog.findOne({
+    title: req.params.blogParams,
   });
+
+  res.render("read", {
+    cssEjs: "read",
+    blogTitle: requestedPost.title,
+    blogImage: requestedPost.picture,
+    blogContent: requestedPost.content,
+  });
+
+  // article.forEach((news) => {
+  //   if (requestedPost === news.title) {
+  //     res.render("read", {
+  //       cssEjs: "read",
+  //       blogTitle: news.title,
+  //       blogImage: news.image,
+  //       blogContent: news.content,
+  //     });
+  //   }
+  // });
 });
 
 module.exports = router;
